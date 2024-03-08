3d0d132087a43f0c2a0629f42f51f16bd21e0c45
Rian <rgillard@nuccimedical.com>
2024-03-08 14:51:13 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const answers = [
+  { msg: "It is certain.", color: "green" },
+  { msg: "It is decidedly so.", color: "green" },
+  { msg: "Without a doubt.", color: "green" },
+  { msg: "Yes - definitely.", color: "green" },
+  { msg: "You may rely on it.", color: "green" },
+  { msg: "As I see it, yes.", color: "green" },
+  { msg: "Most likely.", color: "green" },
+  { msg: "Outlook good.", color: "green" },
+  { msg: "Yes.", color: "green" },
+  { msg: "Signs point to yes.", color: "goldenrod" },
+  { msg: "Reply hazy, try again.", color: "goldenrod" },
+  { msg: "Ask again later.", color: "goldenrod" },
+  { msg: "Better not tell you now.", color: "goldenrod" },
+  { msg: "Cannot predict now.", color: "goldenrod" },
+  { msg: "Concentrate and ask again.", color: "goldenrod" },
+  { msg: "Don't count on it.", color: "red" },
+  { msg: "My reply is no.", color: "red" },
+  { msg: "My sources say no.", color: "red" },
+  { msg: "Outlook not so good.", color: "red" },
+  { msg: "Very doubtful.", color: "red" },
+];
+
+app.get('/api/eightball', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * answers.length);
+    res.json(answers[randomIndex]);
+  });
+  
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+  
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
