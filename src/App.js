57d29a8b61763e970966bdcee05fd8f17fa78eab
Rian <rgillard@nuccimedical.com>
2024-03-08 14:48:28 -0500

    init commit for eightball

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [answer, setAnswer] = useState({ msg: "Think of a Question", color: "black" });
+
+  const handleClick = async () => {
+    const response = await fetch('/api/eightball');
+    const data = await response.json();
+    setAnswer(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="eight-ball"
+        style={{ backgroundColor: answer.color }}
+        onClick={handleClick}
+      >
+        {answer.msg}
+      </div>
     </div>
   );
 }
